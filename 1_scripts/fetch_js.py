22f4ef49417bc1152011a40a283d5f29aca1d3e8
thangckt <thangckt@gmail.com>
2025-12-02 07:32:32 +0000

    Update from https://github.com/thangckt/thkit/commit/2be95e88629529b7e418a594362713e8442f337d

--- 1_scripts/fetch_js.py
+++ 1_scripts/fetch_js.py
@@ -1,19 +1,16 @@
-from pathlib import Path
-
 import requests
 
 
-def download_rawtext(url: str, outfile: str | None = None) -> str:
+def download_rawtext(url: str, outfile: str = None) -> str:
     """Download raw text from a URL."""
     response = requests.get(url)
     if response.status_code == 200:
         text = response.text
     else:
         print(f"Failed to download {url}. HTTP Status: {response.status_code}")
-        text = ""
+        text = None
 
     if text and outfile:
-        Path(outfile).parent.mkdir(parents=True, exist_ok=True)
         with open(outfile, "w") as f:
             f.write(text)
         print(f"File downloaded: {outfile}")

7d2619f4f56b0663c6afc39a85de5e4cfdd962ce
thangckt <thangckt@gmail.com>
2026-03-05 18:46:41 +0000

    Update from https://github.com/thangckt/thkit/commit/bad6fcb411f24e28561bf948234f8462700af4c8

--- 1_scripts/fetch_js.py
+++ 1_scripts/fetch_js.py
@@ -1,30 +1,27 @@
-from pathlib import Path
-
 import requests
 
 
-def download_rawtext(url: str, outfile: str | None = None) -> str:
+def download_rawtext(url: str, outfile: str | None = None) -> str | None:
     """Download raw text from a URL."""
     response = requests.get(url)
     if response.status_code == 200:
         text = response.text
     else:
         print(f"Failed to download {url}. HTTP Status: {response.status_code}")
-        text = ""
+        text = None
 
     if text and outfile:
-        Path(outfile).parent.mkdir(parents=True, exist_ok=True)
         with open(outfile, "w") as f:
             f.write(text)
         print(f"File downloaded: {outfile}")
     return text
 
 
-#####ANCHOR Fetch JS file
+##### ANCHOR: Fetch JS file
 def main():
     download_rawtext(
-        url="https://raw.githubusercontent.com/thangckt/stuff/refs/heads/js_iplog/gas_ip_logger.js",
-        outfile="_docs/1thang_js/gas_ip_logger.js",
+        url="https://raw.githubusercontent.com/thangckt/stuff/refs/heads/js_logVisitor/page_logVisitor.js",
+        outfile="_docs/1thang_js/page_logVisitor.js",
     )
 
 
